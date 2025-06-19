98e6f5ac3ac2928025658df1594c7d07339243cd
Serhat Gultekin <104542830+MehmetSerhatGultekin@users.noreply.github.com>
2025-06-19 15:07:13 +0300

    Initial Commit

---
+++ NewsApp/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Mehmet Serhat Gültekin on 19.06.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
